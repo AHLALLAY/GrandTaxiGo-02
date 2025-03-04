38f347aba3347c945917b35c345e68094ff8c341
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-04 15:12:28 +0000

    feat: Séparer le script de menu mobile

---
+++ public/js/moblie_menu.js
@@ -0,0 +1,4 @@
+document.getElementById('menuButton').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.classList.toggle('hidden');
+});
