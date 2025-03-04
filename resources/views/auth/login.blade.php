e4c4e773e31571bde7256bdb9e4fb5d32508f8d2
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-04 15:13:05 +0000

    feat: Ajouter une button de connection avec google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -90,17 +90,21 @@
                         Se connecter
                     </button>
                 </div>
+                <div class="row">
+
+            </form>
+            <form action="">
+                <div class="w-full md:w-1/4">
+                    <a href="" class="flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-50" role="button">
+                        <!-- <img class="w-5 h-5 mr-2" alt="Google sign-in" src=""> -->
+                        Login with Google
+                    </a>
+                </div>
             </form>
         </div>
     </div>
 
-    <script>
-        // Toggle mobile menu
-        document.getElementById('menuButton').addEventListener('click', function() {
-            const mobileMenu = document.getElementById('mobileMenu');
-            mobileMenu.classList.toggle('hidden');
-        });
-    </script>
+    <script src="{{ asset ('js/mobile_menu.js') }}"></script>
 </body>
 
 </html>
