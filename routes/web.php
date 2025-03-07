94b6d832fe23cb4ab61546ea4c0b28b1975d6130
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-07 07:02:51 +0000

    feat: Ajouter les routes vers l'authentification avec googlr

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\GoogleLoginController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DriverController;
 use App\Http\Controllers\HomeController;
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/google/redirect', [AuthController::class, 'redirectToGoogle'])->name('google.redirect');
+Route::get('/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('google.callback');
 
 // driver
 Route::get('/login', [HomeController::class, 'login'])->name('loginForm');
