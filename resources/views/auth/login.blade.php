9d2ee3e9e1bc9cd390c55b1cb11777fa0c532819
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-03-07 07:04:38 +0000

    del: Supprimer le button de login avec google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -93,14 +93,6 @@
                 <div class="row">
 
             </form>
-            <form action="">
-                <div class="w-full md:w-1/4">
-                    <a href="" class="flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md text-gray-700 hover:bg-gray-50" role="button">
-                        <!-- <img class="w-5 h-5 mr-2" alt="Google sign-in" src=""> -->
-                        Login with Google
-                    </a>
-                </div>
-            </form>
         </div>
     </div>
 
